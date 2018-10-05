e8bfbacbd545eb5e7144df30e294436002f5988d
dosisod <39638017+dosisod@users.noreply.github.com>
2018-10-05 14:28:29 -0700

    pushed failed commit: next song plays on end

--- play.js
+++ play.js
@@ -1,11 +1,11 @@
 window.onload=function() {
 	time=document.getElementById("time")
 	music=document.getElementById("music")
-	setInterval(bar, 60/1000)
-	played=false
+	setInterval(bar, 60/1000) //updates progress bar
+	played=false //loads first song on first load
 	current=0
 	songs=[]
-	tmp=document.getElementsByClassName("song")
+	tmp=document.getElementsByClassName("song") //loads all songs into array
 	for (i=0; i<tmp.length; i++) {
 		songs.push(tmp[i].innerHTML)
 	}
@@ -13,6 +13,7 @@ window.onload=function() {
 }
 function bar() {
 	time.value=music.currentTime
+	if (music.currentTime==music.duration) { next(1) } //plays next song after its done
 }
 function play() {
 	played=true
@@ -23,12 +24,12 @@ function pause() {
 	music.pause()
 	document.getElementById("toggle").src="play.png"
 }
-function toggle() {
+function toggle() { //switches which icon is to be displayed
 	if (!played) { load(songs[0]) }
 	if (music.paused) { play() }
 	else { pause() }
 }
-function next(n) {
+function next(n) { //shifts current index by N, can be any integer
 	if (n<0) {
 		n=songs.length+n
 	}
@@ -45,7 +46,7 @@ function load(s) {
 	}
 }
 function song(e) {
-	if (e.target.tagName.toLowerCase() == "p") {
+	if (e.target.tagName.toLowerCase() == "p") { //dont load song if div is clicked
 		load(e.target.innerHTML)
 		for (i=0; i<songs.length; i++) {
 			if (songs[i]==e.target.innerHTML) {
